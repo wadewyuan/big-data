4eb3f885c6b37b152a067b429e6a1077d2c2c961
Wade Yuan <wadeyuanwy@hotmail.com>
2018-01-05 18:00:54 +0800

    Added JWT Authentication. TBD

--- src/main/java/com/citictel/bigdata/domain/Hello.java
+++ src/main/java/com/citictel/bigdata/domain/Hello.java
@@ -19,7 +19,7 @@ public class Hello {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(length = 128, unique = true)
+    @Column(length = 128)
     private String name;
 
     public Long getId() {
