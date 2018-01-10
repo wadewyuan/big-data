f715ed120faed6f2e6432e7295084cf6a0fd04d5
Wade Yuan <wadeyuanwy@hotmail.com>
2018-01-10 22:37:37 +0800

    Optimize import

--- src/main/java/com/citictel/bigdata/configuration/SecurityConfig.java
+++ src/main/java/com/citictel/bigdata/configuration/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.citictel.bigdata.filter.JWTLoginFilter;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
