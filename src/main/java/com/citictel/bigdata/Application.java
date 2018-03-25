27e1f2207214c89f7db3d2529734822ee823193e
Wade Yuan <wadeyuanwy@hotmail.com>
2018-03-25 19:48:15 +0800

    enable web mvc and throw exception when no handler found

--- src/main/java/com/citictel/bigdata/Application.java
+++ src/main/java/com/citictel/bigdata/Application.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
